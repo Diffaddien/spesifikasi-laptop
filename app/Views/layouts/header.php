8d2ed5510ac470d03376a0e0fe09c84a26b3737f
Diffaddien <diffa.addien101519@students.unila.ac.id>
2021-11-21 20:23:31 +0700

    update styles

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -8,6 +8,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    
+    <!-- Ambil aset CSS -->
+    <link href="/assets/styles.css" rel="stylesheet" type="text/css">
+    <script src="/assets/jquery.js" type='text/javascript'></script>
+
 
     <title> <?php echo $title; ?> </title>
 </head>
--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">Blog App</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,4 +18,36 @@
             </ul>
         </div>
     </div>
+</nav> -->
+
+<!-- header -->
+<nav>
+  <ul class="menu">
+    <li class="logo"><a href="#">Creative Mind Agency</a></li>
+    <li class="item"><a href="#">Home</a></li>
+    <li class="item"><a href="#">About</a></li>
+    <li class="item has-submenu">
+      <a tabindex="0">Services</a>
+      <ul class="submenu">
+        <li class="subitem"><a href="#">Design</a></li>
+        <li class="subitem"><a href="#">Development</a></li>
+        <li class="subitem"><a href="#">SEO</a></li>
+        <li class="subitem"><a href="#">Copywriting</a></li>
+      </ul>
+    </li>
+    <li class="item has-submenu">
+      <a tabindex="0">Plans</a>
+      <ul class="submenu">
+        <li class="subitem"><a href="#">Freelancer</a></li>
+        <li class="subitem"><a href="#">Startup</a></li>
+        <li class="subitem"><a href="#">Enterprise</a></li>
+      </ul>
+    </li>
+    <li class="item"><a href="#">Blog</a></li>
+    <li class="item"><a href="#">Contact</a>
+    </li>
+    <li class="item button"><a href="#">Log In</a></li>
+    <li class="item button secondary"><a href="#">Sign Up</a></li>
+    <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+  </ul>
 </nav>
