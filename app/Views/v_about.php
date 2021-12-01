deedf3abbad86eb633a8a7dd37cc369b9c853749
Diffa Addien Aziz <73152172+Diffaddien@users.noreply.github.com>
2021-12-01 22:31:34 +0700

    Update v_about.php

--- app/Views/v_about.php
+++ app/Views/v_about.php
@@ -1,23 +1,3 @@
 <div class="container">
-
-  <br>
-<div class="jumbotron text-center">
-<img src="https://drive.google.com/uc?export=view&id=1GyeXL9g_49gh2EUT4He2NUWy-b9xfs2t"  width="200" 
-class="rounded-circle"/>
-  <h3 class="text-center">Diffa Addien Aziz</h3>
-  <h4 class="text-center">1917051015</h4>
-  <hr class="my-4">
-  <img src="https://drive.google.com/uc?export=view&id=142hMYqhfhhF2cQvB2sJcnU6uRjS-jhKf"  width="200"
-  
-class="rounded-circle"/>
-  <h3 class="text-center">Hendri Kurniawan</h3>
-  <h4 class="text-center">1957051007</h4>
-  <hr class="my-4">
-  <img src="https://drive.google.com/uc?export=view&id=1NsrsI0W2ZN1B_QjP5v6yZDPUxCK98oHc"  width="200"
-class="rounded-circle"/>
-  <h3 class="text-center">Nanda Bagus Pratama</h3>
-  <h4 class="text-center">1917051033</h4>
-  <hr class="my-4">
-  </div>
-  
+ini adalah halaman About
 </div>
